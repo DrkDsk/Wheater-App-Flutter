0ae7120fa59feffc41188e5a3db558cbadc299e0
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-09 14:15:14 -0600

    feat: updated dependency injection for use cases and services

--- lib/core/di/di_use_cases.dart
+++ lib/core/di/di_use_cases.dart
@@ -1,5 +1,5 @@
+import 'package:clima_app/features/home/domain/repositories/location_repository.dart';
 import 'package:clima_app/features/home/domain/repositories/search_weather_repository.dart';
-import 'package:clima_app/features/home/domain/services/location_service.dart';
 import 'package:clima_app/features/home/domain/usecases/get_weather_use_case.dart';
 import 'package:clima_app/features/home/presentation/dto/weather_mapper.dart';
 import 'package:get_it/get_it.dart';
@@ -9,9 +9,9 @@ final getIt = GetIt.instance;
 Future registerUseCases() async {
   getIt.registerLazySingleton<GetWeatherUseCase>(
     () => GetWeatherUseCase(
-      repository: getIt<SearchWeatherRepository>(),
-      locationService: getIt<LocationService>(),
+      searchWeatherRepository: getIt<SearchWeatherRepository>(),
       mapper: getIt<WeatherMapper>(),
+      locationRepository: getIt<LocationRepository>(),
     ),
   );
 }
