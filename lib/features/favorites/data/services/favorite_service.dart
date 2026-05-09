c581c50a5a0f118ca38433b3663327fb1a25d2a4
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-09 14:15:36 -0600

    refactor: replaced FavoriteService with LocationMapper and removed location cache logic

--- lib/features/favorites/data/services/favorite_service.dart
+++ lib/features/favorites/data/services/favorite_service.dart
@@ -1,40 +1,10 @@
 import 'package:clima_app/features/favorites/data/datasources/favorite_weather_datasource.dart';
-import 'package:clima_app/features/favorites/data/models/location_cache_hive_model.dart';
-import 'package:clima_app/features/home/domain/services/location_service.dart';
 
-class FavoriteService {
-  final FavoriteWeatherDataSource _favoriteWeatherDataSource;
-  final LocationService _locationService;
-
-  const FavoriteService({
+class LocationMapper {
+  const LocationMapper({
     required FavoriteWeatherDataSource favoriteDataSource,
-    required LocationService locationService,
-  })  : _favoriteWeatherDataSource = favoriteDataSource,
-        _locationService = locationService;
+  });
 
-  Future<LocationCacheHiveModel> getLocationCache() async {
-    final storedLocationCache =
-        await _favoriteWeatherDataSource.getStoredLocationCache();
-
-    final currentCoordinates = await _locationService.ensureCoordinates();
-    final currentLat = roundCoordinate(currentCoordinates.latitude);
-    final currentLon = roundCoordinate(currentCoordinates.longitude);
-
-    final hasStoredLocation = storedLocationCache != null;
-    final hasMoved = hasStoredLocation &&
-        (storedLocationCache.latitude != currentLat ||
-            storedLocationCache.longitude != currentLon);
-
-    if (hasStoredLocation && !hasMoved) {
-      return storedLocationCache;
-    }
-
-    final currentCityLocation =
-        await _locationService.getCityNameFromCoordinates();
-
-    return LocationCacheHiveModel.fromEntity(currentCityLocation);
-  }
-
-  double roundCoordinate(double value) =>
+  static double roundCoordinate(double value) =>
       double.parse(value.toStringAsFixed(3));
 }
