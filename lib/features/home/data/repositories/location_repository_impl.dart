009cb7b568be05bb9bb29379e61e2e6394724473
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-09 14:14:46 -0600

    feat: add ensureCoordinates to LocationRepository

--- lib/features/home/data/repositories/location_repository_impl.dart
+++ lib/features/home/data/repositories/location_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:clima_app/core/error/exceptions/unknown_exception.dart';
 import 'package:clima_app/core/shared/data/datasources/location_datasource_impl.dart';
 import 'package:clima_app/features/home/domain/entities/coordinate.dart';
 import 'package:clima_app/features/home/domain/repositories/location_repository.dart';
@@ -11,8 +12,6 @@ class LocationRepositoryImpl implements LocationRepository {
   @override
   Future<Coordinate?> getCurrentLocation() async {
     final position = await dataSource.getCurrentLocation();
-
-    if (position == null) return null;
 
     return Coordinate(
       latitude: position.latitude ?? 0,
@@ -29,5 +28,30 @@ class LocationRepositoryImpl implements LocationRepository {
     );
 
     return placemarks.isEmpty ? null : placemarks.first;
+  }
+
+  @override
+  Future<Coordinate> ensureCoordinates({
+    double? latitude,
+    double? longitude,
+  }) async {
+    if (latitude != null && longitude != null) {
+      return Coordinate(
+        latitude: latitude,
+        longitude: longitude,
+      );
+    }
+
+    final coordinate = await getCurrentLocation();
+    if (coordinate == null) {
+      throw UnknownException(
+        message: "No se ha podido obtener la ubicación del usuario.",
+      );
+    }
+
+    return Coordinate(
+      latitude: coordinate.latitude,
+      longitude: coordinate.longitude,
+    );
   }
 }
