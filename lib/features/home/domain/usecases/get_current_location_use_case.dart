2edd76b2b0b8a25bbddcec693dab7cb9dca74c9b
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-09 23:06:51 -0600

    refactor: updated return type to UserLocation in GetFavoritesAndCurrentLocationUseCase

--- lib/features/home/domain/usecases/get_current_location_use_case.dart
+++ lib/features/home/domain/usecases/get_current_location_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:clima_app/core/error/failures/failure.dart';
-import 'package:clima_app/features/city/domain/entities/city_location.dart';
+import 'package:clima_app/features/city/domain/entities/user_location.dart';
 import 'package:clima_app/features/favorites/domain/repository/favorite_repository.dart';
 import 'package:clima_app/features/home/domain/repositories/location_repository.dart';
 import 'package:dartz/dartz.dart';
@@ -8,13 +8,13 @@ class GetFavoritesAndCurrentLocationUseCase {
   final LocationRepository _locationRepository;
   final FavoriteRepository _favoriteRepository;
 
-  const GetFavoritesAndCurrentLocationUseCase(
-      {required LocationRepository locationRepository,
-      required FavoriteRepository favoriteRepository})
-      : _locationRepository = locationRepository,
+  const GetFavoritesAndCurrentLocationUseCase({
+    required LocationRepository locationRepository,
+    required FavoriteRepository favoriteRepository,
+  })  : _locationRepository = locationRepository,
         _favoriteRepository = favoriteRepository;
 
-  Future<Either<Failure, List<CityLocation>>> call() async {
+  Future<Either<Failure, List<UserLocation>>> call() async {
     final (locationCached, favoritesCities) = await (
       _locationRepository.getCurrentLocation(),
       _favoriteRepository.index()
