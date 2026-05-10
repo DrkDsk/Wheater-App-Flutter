ead78a0d1a96ac5a3247e44dcdd868b6ca34d6bc
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-09 20:29:50 -0600

    refactor: remove NetworkHelper and unused methods from location data sources and repositories

--- lib/features/home/domain/repositories/location_repository.dart
+++ lib/features/home/domain/repositories/location_repository.dart
@@ -1,5 +1,4 @@
 import 'package:clima_app/features/city/domain/entities/city_location.dart';
-import 'package:clima_app/features/home/domain/entities/coordinate.dart';
 import 'package:geocoding/geocoding.dart';
 
 abstract class LocationRepository {
@@ -8,10 +7,5 @@ abstract class LocationRepository {
   Future<Placemark?> getLocationInformation({
     required double latitude,
     required double longitude,
-  });
-
-  Future<Coordinate> ensureCoordinates({
-    double? latitude,
-    double? longitude,
   });
 }
