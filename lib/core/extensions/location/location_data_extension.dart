153a312e396310733e0a2adf1575d94802092267
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-09 20:17:38 -0600

    refactor: updated location_data_extension import to city_location.dart

--- lib/core/extensions/location/location_data_extension.dart
+++ lib/core/extensions/location/location_data_extension.dart
@@ -1,4 +1,4 @@
-import 'package:clima_app/features/city/domain/entities/city_location_entity.dart';
+import 'package:clima_app/features/city/domain/entities/city_location.dart';
 import 'package:location/location.dart';
 
 extension LocationDataExtension on LocationData {
