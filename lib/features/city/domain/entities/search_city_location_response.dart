1f612fc5fe86d2ad34623c4a9eb0f72e25d46df7
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-09 20:17:11 -0600

    feat: search_city_location_response modified

--- lib/features/city/domain/entities/search_city_location_response.dart
+++ lib/features/city/domain/entities/search_city_location_response.dart
@@ -1,8 +1,7 @@
-import 'package:clima_app/features/city/domain/entities/city_location_entity.dart';
+import 'package:clima_app/features/city/domain/entities/city_location.dart';
 
-class SearchCityLocationResponse{
+class SearchCityLocationResponse {
   final List<CityLocation> data;
 
   const SearchCityLocationResponse({required this.data});
-
 }
