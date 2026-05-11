c8267324aa2c4bde7e23636b40d70b87f5892f68
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-11 14:16:41 -0600

    feat: store location use case created

---
+++ lib/features/city/domain/use_cases/store_location_use_case.dart
@@ -0,0 +1,24 @@
+import 'package:clima_app/core/error/failures/failure.dart';
+import 'package:clima_app/features/city/domain/entities/city_location.dart';
+import 'package:clima_app/features/home/domain/repositories/location_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class StoreLocationUseCase {
+  final LocationRepository _locationRepository;
+
+  const StoreLocationUseCase({
+    required LocationRepository locationRepository,
+  }) : _locationRepository = locationRepository;
+
+  Future<Either<Failure, CityLocation>> call(CityLocation location) async {
+    try {
+      final request = await _locationRepository.store(location);
+
+      return Right(request);
+    } catch (e) {
+      return Left(UnexpectedFailure(
+        "Ocurrió un error al guardar la ubicación",
+      ));
+    }
+  }
+}
