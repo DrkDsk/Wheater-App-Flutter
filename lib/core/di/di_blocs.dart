ad6a7e8a36be00df12e20ddfbf6e05165720d442
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-11 13:29:56 -0600

    feat: register and inject ObserveLocationChangesUseCase

--- lib/core/di/di_blocs.dart
+++ lib/core/di/di_blocs.dart
@@ -5,6 +5,7 @@ import 'package:clima_app/features/favorites/domain/repository/favorite_reposito
 import 'package:clima_app/features/favorites/presentation/fetch/cubits/favorite_cubit.dart';
 import 'package:clima_app/features/home/domain/usecases/get_current_location_use_case.dart';
 import 'package:clima_app/features/home/domain/usecases/get_weather_use_case.dart';
+import 'package:clima_app/features/home/domain/usecases/observe_location_changes_use_case.dart';
 import 'package:clima_app/features/home/presentation/blocs/city_weather_bloc.dart';
 import 'package:clima_app/features/home/presentation/blocs/home_page_navigation_cubit.dart';
 import 'package:clima_app/features/ia/domain/repositories/ia_repository.dart';
@@ -26,6 +27,7 @@ Future registerBlocs() async {
     () => FavoriteCubit(
       repository: getIt<FavoriteRepository>(),
       favoritesUseCase: getIt<GetFavoritesAndCurrentLocationUseCase>(),
+      locationWatchUseCase: getIt<ObserveLocationChangesUseCase>(),
     ),
   );
 
--- lib/core/di/di_use_cases.dart
+++ lib/core/di/di_use_cases.dart
@@ -3,12 +3,19 @@ import 'package:clima_app/features/home/domain/repositories/location_repository.
 import 'package:clima_app/features/home/domain/repositories/search_weather_repository.dart';
 import 'package:clima_app/features/home/domain/usecases/get_current_location_use_case.dart';
 import 'package:clima_app/features/home/domain/usecases/get_weather_use_case.dart';
+import 'package:clima_app/features/home/domain/usecases/observe_location_changes_use_case.dart';
 import 'package:clima_app/features/home/presentation/dto/weather_mapper.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
 Future registerUseCases() async {
+  getIt.registerLazySingleton<ObserveLocationChangesUseCase>(
+    () => ObserveLocationChangesUseCase(
+      locationRepository: getIt<LocationRepository>(),
+    ),
+  );
+
   getIt.registerLazySingleton<GetFavoritesAndCurrentLocationUseCase>(
     () => GetFavoritesAndCurrentLocationUseCase(
       locationRepository: getIt<LocationRepository>(),
