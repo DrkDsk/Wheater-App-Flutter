fdd8a2a2295b9d86e6de77115d48af4658c455e5
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-13 09:24:22 -0600

    refactor: removed StoreCityBloc from dependency injection
    - Removed `StoreCityBloc` and `StoreLocationUseCase` from `di_blocs.dart`.
    - Cleaned up unused imports and factory registrations in the `registerBlocs` function.

--- lib/core/di/di_blocs.dart
+++ lib/core/di/di_blocs.dart
@@ -1,7 +1,6 @@
 import 'package:clima_app/core/helpers/network_helper.dart';
 import 'package:clima_app/core/shared/ui/cubits/network_cubit.dart';
 import 'package:clima_app/features/city/domain/repositories/city_repository.dart';
-import 'package:clima_app/features/city/domain/use_cases/store_location_use_case.dart';
 import 'package:clima_app/features/favorites/domain/repository/favorite_repository.dart';
 import 'package:clima_app/features/favorites/presentation/fetch/cubits/favorite_cubit.dart';
 import 'package:clima_app/features/home/domain/usecases/get_current_location_use_case.dart';
@@ -9,7 +8,6 @@ import 'package:clima_app/features/home/domain/usecases/get_weather_use_case.dar
 import 'package:clima_app/features/home/domain/usecases/observe_location_changes_use_case.dart';
 import 'package:clima_app/features/home/presentation/blocs/city_weather_bloc.dart';
 import 'package:clima_app/features/home/presentation/blocs/home_page_navigation_cubit.dart';
-import 'package:clima_app/features/home/presentation/blocs/store_city_bloc.dart';
 import 'package:clima_app/features/ia/domain/repositories/ia_repository.dart';
 import 'package:clima_app/features/ia/ui/blocs/ia_cubit.dart';
 import 'package:get_it/get_it.dart';
@@ -18,7 +16,6 @@ final getIt = GetIt.instance;
 
 Future registerBlocs() async {
   final observeLocationUseCase = getIt<ObserveLocationChangesUseCase>();
-  final storeCityLocation = getIt<StoreLocationUseCase>();
 
   getIt.registerFactory<NetworkCubit>(
     () => NetworkCubit(networkService: getIt<NetworkHelper>()),
@@ -45,12 +42,5 @@ Future registerBlocs() async {
 
   getIt.registerFactory<HomePageNavigationCubit>(
     () => HomePageNavigationCubit(),
-  );
-
-  getIt.registerFactory<StoreCityBloc>(
-    () => StoreCityBloc(
-      storeLocationUseCase: storeCityLocation,
-      observerLocationUseCase: observeLocationUseCase,
-    ),
   );
 }
