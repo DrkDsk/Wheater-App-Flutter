017f9fa14847436c8d432fb6103bc6b50c9bf87d
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-12 21:45:17 -0600

    feat: register StoreCityBloc in di_blocs.dart
    - Added `StoreCityBloc` factory registration using `StoreLocationUseCase` and `ObserveLocationChangesUseCase`.
    - Extracted `ObserveLocationChangesUseCase` to a variable to be shared between `CityWeatherBloc` and `StoreCityBloc`.
    - Imported required dependencies for the new bloc registration.

--- lib/core/di/di_blocs.dart
+++ lib/core/di/di_blocs.dart
@@ -1,6 +1,7 @@
 import 'package:clima_app/core/helpers/network_helper.dart';
 import 'package:clima_app/core/shared/ui/cubits/network_cubit.dart';
 import 'package:clima_app/features/city/domain/repositories/city_repository.dart';
+import 'package:clima_app/features/city/domain/use_cases/store_location_use_case.dart';
 import 'package:clima_app/features/favorites/domain/repository/favorite_repository.dart';
 import 'package:clima_app/features/favorites/presentation/fetch/cubits/favorite_cubit.dart';
 import 'package:clima_app/features/home/domain/usecases/get_current_location_use_case.dart';
@@ -8,6 +9,7 @@ import 'package:clima_app/features/home/domain/usecases/get_weather_use_case.dar
 import 'package:clima_app/features/home/domain/usecases/observe_location_changes_use_case.dart';
 import 'package:clima_app/features/home/presentation/blocs/city_weather_bloc.dart';
 import 'package:clima_app/features/home/presentation/blocs/home_page_navigation_cubit.dart';
+import 'package:clima_app/features/home/presentation/blocs/store_city_bloc.dart';
 import 'package:clima_app/features/ia/domain/repositories/ia_repository.dart';
 import 'package:clima_app/features/ia/ui/blocs/ia_cubit.dart';
 import 'package:get_it/get_it.dart';
@@ -15,6 +17,9 @@ import 'package:get_it/get_it.dart';
 final getIt = GetIt.instance;
 
 Future registerBlocs() async {
+  final observeLocationUseCase = getIt<ObserveLocationChangesUseCase>();
+  final storeCityLocation = getIt<StoreLocationUseCase>();
+
   getIt.registerFactory<NetworkCubit>(
     () => NetworkCubit(networkService: getIt<NetworkHelper>()),
   );
@@ -34,11 +39,18 @@ Future registerBlocs() async {
     () => CityWeatherBloc(
       getWeatherUseCase: getIt<GetWeatherUseCase>(),
       cityRepository: getIt<CityRepository>(),
-      locationWatchUseCase: getIt<ObserveLocationChangesUseCase>(),
+      locationWatchUseCase: observeLocationUseCase,
     ),
   );
 
   getIt.registerFactory<HomePageNavigationCubit>(
     () => HomePageNavigationCubit(),
+  );
+
+  getIt.registerFactory<StoreCityBloc>(
+    () => StoreCityBloc(
+      storeLocationUseCase: storeCityLocation,
+      observerLocationUseCase: observeLocationUseCase,
+    ),
   );
 }
