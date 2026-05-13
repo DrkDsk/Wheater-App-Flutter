9dbc4c6c3d0a31845ece2ba4a301b4cbe63a076d
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-13 10:38:17 -0600

    refactor: renamed favorite_fetch_state.dart and simplified FavoriteState
    - Renamed `favorite_fetch_state.dart` to `favorite_state.dart`.
    - Removed `cities` list and its corresponding import from `FavoriteState`.
    - Updated `copyWith` and `props` to reflect the removal of the `cities` property.

--- lib/features/favorites/presentation/fetch/cubits/favorite_state.dart
+++ lib/features/favorites/presentation/fetch/cubits/favorite_state.dart
@@ -1,30 +1,25 @@
-import 'package:clima_app/features/city/domain/entities/city_location.dart';
 import 'package:equatable/equatable.dart';
 
 enum FavoriteStatus { initial, loading, success, failure }
 
 class FavoriteState with EquatableMixin {
   final FavoriteStatus status;
-  final List<CityLocation> cities;
   final String message;
   final bool isAvailableToStore;
 
   const FavoriteState({
     this.status = FavoriteStatus.initial,
-    this.cities = const [],
     this.isAvailableToStore = false,
     this.message = "",
   });
 
   FavoriteState copyWith({
     FavoriteStatus? status,
-    List<CityLocation>? cities,
     String? message,
     bool? isAvailableToStore,
   }) {
     return FavoriteState(
       status: status ?? this.status,
-      cities: cities ?? this.cities,
       message: message ?? this.message,
       isAvailableToStore: isAvailableToStore ?? this.isAvailableToStore,
     );
@@ -33,7 +28,6 @@ class FavoriteState with EquatableMixin {
   @override
   List<Object?> get props => [
         status,
-        cities,
         message,
         isAvailableToStore,
       ];
