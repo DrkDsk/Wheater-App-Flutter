f7ce135b1736222116d5c679858e6525b5ebbdcf
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-13 11:13:08 -0600

    refactor: update WeatherListItem to use coordinates and remove forecast
    - Replaced `forecast` property with `coordinate` in `CurrentLocationItem`.
    - Removed the optional `forecast` property from `FavoriteWeatherItem`.
    - Updated imports to use `Coordinate` instead of `Forecast` entity.

--- lib/features/home/presentation/weather_list_item.dart
+++ lib/features/home/presentation/weather_list_item.dart
@@ -1,22 +1,20 @@
 import 'package:clima_app/features/city/domain/entities/city_location.dart';
-import 'package:clima_app/features/home/domain/entities/forecast.dart';
+import 'package:clima_app/features/home/domain/entities/coordinate.dart';
 
 sealed class WeatherListItem {
   const WeatherListItem();
 }
 
 class CurrentLocationItem extends WeatherListItem {
-  final Forecast forecast;
+  final Coordinate coordinate;
 
-  const CurrentLocationItem({required this.forecast});
+  const CurrentLocationItem({required this.coordinate});
 }
 
 class FavoriteWeatherItem extends WeatherListItem {
   final CityLocation cityLocation;
-  final Forecast? forecast;
 
   const FavoriteWeatherItem({
     required this.cityLocation,
-    this.forecast,
   });
 }
