31da4cd48d02789022c4f3e14b4c1b251b90656b
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-13 12:20:49 -0600

    refactor: update ObserveLocationChangesUseCase to use GeoLocatorRepository
    - Replaced `LocationRepository` with `GeoLocatorRepository` in `ObserveLocationChangesUseCase`.
    - Updated the constructor and class member to reflect the repository change.

--- lib/features/home/domain/usecases/observe_location_changes_use_case.dart
+++ lib/features/home/domain/usecases/observe_location_changes_use_case.dart
@@ -1,11 +1,11 @@
 import 'package:clima_app/features/city/domain/entities/city_location.dart';
-import 'package:clima_app/features/home/domain/repositories/location_repository.dart';
+import 'package:clima_app/features/home/domain/repositories/geo_locator_repository.dart';
 
 class ObserveLocationChangesUseCase {
-  final LocationRepository _repository;
+  final GeoLocatorRepository _repository;
 
   ObserveLocationChangesUseCase({
-    required LocationRepository locationRepository,
+    required GeoLocatorRepository locationRepository,
   }) : _repository = locationRepository;
 
   Stream<CityLocation> call() {
