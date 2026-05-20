45a9d19caa85a56c1285ff267a7ff1e75f7ceb78
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-20 14:33:59 -0600

    refactor: remove unused Current parameter from SkyGradientStrategy
    - Removed the `Current` entity import from `sky_gradient_strategy.dart`.
    - Updated the `resolve` method signature to remove the unnecessary `current` parameter.

--- lib/features/weather/domain/strategy/sky_gradient_strategy.dart
+++ lib/features/weather/domain/strategy/sky_gradient_strategy.dart
@@ -1,4 +1,3 @@
-import 'package:clima_app/features/home/domain/entities/current.dart';
 import 'package:clima_app/features/weather/domain/entities/sky_atmosphere_metrics.dart';
 import 'package:clima_app/features/weather/presentation/animations/configs/sky_gradient_config.dart';
 import 'package:clima_app/features/weather/presentation/layers/palettes/sky_palette.dart';
@@ -7,7 +6,6 @@ abstract class SkyGradientStrategy {
   SkyGradientConfig resolve({
     required SkyPalette palette,
     required SkyAtmosphereMetrics metrics,
-    required Current current,
     required bool isNight,
   });
 }
