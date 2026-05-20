77959c8462048d308e27d5b845c3f8223fbaa2a9
Alfredo Palacios <joseaph.1998@gmail.com>
2026-05-19 21:09:10 -0600

    feat: implement specific sky palettes for weather conditions
    - Created `ClearSkyPalette`, `OvercastPalette`, `CloudyPalette`, and `FewCloudsPalette` classes.
    - Defined color properties for each palette by extending the `SkyPalette` base class to support various atmospheric layers.

---
+++ lib/features/weather/presentation/layers/palettes/sky_palette.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+abstract class SkyPalette {
+  Color get topSkyA;
+
+  Color get topSkyB;
+
+  Color get warmA;
+
+  Color get warmB;
+
+  Color get horizonA;
+
+  Color get horizonB;
+
+  Color get radialA;
+
+  Color get radialB;
+}
