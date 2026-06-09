d69188e15c680736913e86a821916ac1a5ecb12e
Alfredo Palacios <joseaph.1998@gmail.com>
2026-06-08 18:58:35 -0600

    Revertir últimos 2 commits

--- lib/core/shared/data/datasources/weather_description_local_datasource.dart
+++ lib/core/shared/data/datasources/weather_description_local_datasource.dart
@@ -1,18 +1,15 @@
 import 'dart:convert';
-import 'dart:isolate';
 import 'package:clima_app/features/home/domain/entities/translated/translated_weather.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 class WeatherDescriptionLocalDataSourceImpl {
   Future<List<TranslatedWeather>> loadDescriptions() async {
-    final String jsonString = await rootBundle.loadString(
-      'assets/json/weather_descriptions.json',
-    );
+    final String jsonString =
+        await rootBundle.loadString('assets/json/weather_descriptions.json');
+    final List<dynamic> jsonList = json.decode(jsonString);
 
-    return Isolate.run(() {
-      final List<dynamic> jsonList = json.decode(jsonString);
-
-      return jsonList.map((e) => TranslatedWeather.fromJson(e)).toList();
-    });
+    return jsonList
+        .map((jsonItem) => TranslatedWeather.fromJson(jsonItem))
+        .toList();
   }
 }
